c48947127881f5692121fd41aab4ea102fea473e
Victor Lucio <>
2023-05-08 10:13:59 -0300

    Adicionando o código

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 """This program counts the number of vowels in a string."""
 
 def count_vowels(string:str) -> int:
+    vogais = 0
+    for letter in string:
+        if letter.lower() == 'a' or letter.lower() == 'e' or letter.lower() == 'i' or letter.lower() == 'o' or letter.lower() == 'u':
+            vogais += 1
+    return vogais
     """
     Takes in a string and returns the number of vowels in the string.
 
